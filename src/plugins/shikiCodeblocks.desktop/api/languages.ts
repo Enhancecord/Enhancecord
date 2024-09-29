794e13bc68226c7dae7c2045ca90aaf1a1a5d1a2
0xJWLabs <0xJWLabs@gmail.com>
2024-09-29 08:17:14 +0700

    chore: update shiki

--- src/plugins/shikiCodeblocks.desktop/api/languages.ts
+++ src/plugins/shikiCodeblocks.desktop/api/languages.ts
@@ -18,9 +18,10 @@
 
 import { ILanguageRegistration } from "@vap/shiki";
 
-export const VPC_REPO = "Vencordian/grammars";
-export const VPC_REPO_COMMIT = "61c67b04a59d838feb5a70de03c3bb92c1f59d05";
+export const VPC_REPO = "Vap0r1ze/vapcord";
+export const VPC_REPO_COMMIT = "88a7032a59cca40da170926651b08201ea3b965a";
 export const vpcRepoAssets = `https://raw.githubusercontent.com/${VPC_REPO}/${VPC_REPO_COMMIT}/assets/shiki-codeblocks`;
+
 export const vpcRepoGrammar = (fileName: string) => `${vpcRepoAssets}/${fileName}`;
 export const vpcRepoLanguages = `${vpcRepoAssets}/languages.json`;
 
